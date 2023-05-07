094f082e33134d2c29130000c763eecf16950f1d
woprandis09 <bkpecho05@gmail.com>
2023-05-07 23:27:10 +0800

    feat: add form element
    add form element in index.html and implement post '/quotes' in server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/quotes', (req, res) => {
+  console.log('Hello');
+});
+
 app.listen(3000, function () {
   console.log('listening on 3000');
 });
