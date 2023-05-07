a41d24177a19c18562cf022ac6d1c57c9a285472
woprandis09 <bkpecho05@gmail.com>
2023-05-07 23:07:13 +0800

    feat: add index.html and serve it with res.sendFile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, function () {
