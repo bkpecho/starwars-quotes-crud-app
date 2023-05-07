d4a68b27f339fffae8e66d574a150ae1c7640753
woprandis09 <bkpecho05@gmail.com>
2023-05-07 23:33:39 +0800

    chore: add body-parser package to server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
