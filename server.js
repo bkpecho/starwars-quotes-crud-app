0bb340eb18ca59387ae48ff9ecb4efd146b37234
woprandis09 <bkpecho05@gmail.com>
2023-05-11 16:43:38 +0800

    feat: create quotes collection

--- server.js
+++ server.js
@@ -10,13 +10,22 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     console.log('Connected to Database');
     const db = client.db('star-wars-quotes');
 
+    const quotesCollection = db.collection('quotes');
+
     app.use(bodyParser.urlencoded({ extended: true }));
     app.get('/', (req, res) => {
       res.sendFile(__dirname + '/index.html');
     });
+
     app.post('/quotes', (req, res) => {
-      console.log(req.body);
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.log(error));
     });
+
     app.listen(3000, function () {
       console.log('listening on 3000');
     });
