10c1dd8b597be770ff6f297bbbb7e8457dfb624d
woprandis09 <bkpecho05@gmail.com>
2023-05-11 15:30:52 +0800

    chore: move express request handlers into MongoClient's then call

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const connectionString = 'mongodb+srv://bkpecho:LZP2c6jBRMfvlvS8@cluster0.ef2gdjb.mongodb.net/?retryWrites=true&w=majority'
+const connectionString =
+  'mongodb+srv://bkpecho:LZP2c6jBRMfvlvS8@cluster0.ef2gdjb.mongodb.net/?retryWrites=true&w=majority';
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
+  .then((client) => {
+    console.log('Connected to Database');
+    const db = client.db('star-wars-quotes');
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
+    });
+    app.post('/quotes', (req, res) => {
+      console.log(req.body);
+    });
+    app.listen(3000, function () {
+      console.log('listening on 3000');
+    });
   })
-  .catch(error => console.error(error))
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/quotes', (req, res) => {
-  console.log(req.body);
-});
-
-app.listen(3000, function () {
-  console.log('listening on 3000');
-});
+  .catch((error) => console.error(error));
