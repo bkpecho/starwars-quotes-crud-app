14d7f4c3a3813d78f5b598132b16f703cceb1493
woprandis09 <bkpecho05@gmail.com>
2023-05-12 21:37:32 +0800

    feat: create fetch api to trigger put request

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
 const update = document.querySelector('#update-button');
 
-update.addEventListener('click', () => {});
+update.addEventListener('click', () => {
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vader',
+      quote: 'I find your lack of faith disturbing.'
+    })
+  });
+});
