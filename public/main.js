a10d258d3ea724c5be7eecd04cd3f51ccda45ac4
Bryan King Pecho <bkpecho05@gmail.com>
2023-05-14 14:35:17 +0800

    chore: add  method to update's eventListener

--- public/main.js
+++ public/main.js
@@ -8,5 +8,15 @@ update.addEventListener('click', () => {
       name: 'Darth Vader',
       quote: 'I find your lack of faith disturbing.'
     })
-  });
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+    })
+    .then((response) => {
+      console.log(response);
+      window.location.reload(true);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
